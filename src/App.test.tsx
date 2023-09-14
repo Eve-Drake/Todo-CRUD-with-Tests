ef0179d12d43c4bdd364c619d063e4540d09a1da
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2023-09-14 16:42:59 +0100

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,53 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+/* eslint-disable testing-library/prefer-screen-queries */
+import { fireEvent, getByText, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+
+test('task list starts empty', () =>{
+  render(<App />)
+  const addButton = screen.getByText('Add')
+  expect(addButton).toBeInTheDocument()
+})
+
+test('adding a task', () =>{
+  render(<App />)
+  const input = screen.getByLabelText('Task:')
+  const addBtn = screen.getByText('Add')
+
+  fireEvent.change(input, {target: {value: 'New Task'}})
+  fireEvent.click(addBtn)
+
+  const newTask = screen.getByText('New Task')
+
+  expect(newTask).toBeInTheDocument()
+})
+
+
+test('delete a task', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const input = screen.getByLabelText('Task:');
+  const addBtn = screen.getByText('Add');
+
+  fireEvent.change(input, { target: { value: 'New Task' } });
+  fireEvent.click(addBtn);
+
+  const delBtn = screen.getByText('Delete');
+
+  fireEvent.click(delBtn);
+  const deletedTask = screen.queryByText('New Task');
+
+  expect(deletedTask).toBeNull();
 });
+
+test('Complete a Task', () =>{
+  render(<App />)
+  const input = screen.getByLabelText('Task:')
+  const addBtn = screen.getByText('Add')
+  fireEvent.change(input, {target: {value: 'New Task'}})
+  fireEvent.click(addBtn)
+  const complete = screen.getByText('Mark Complete')
+  fireEvent.click(complete)
+  const newTask = screen.getByText('New Task')
+  expect(newTask).toHaveClass('complete')
+})
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+interface Task{
+  task: string,
+  id: number,
+  complete: boolean
+}
+
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([])
+  const [taskText, setTaskText] = useState<string>('')
+
+
+  const addTask = () =>{
+    if(taskText){
+      setTasks([...tasks, {task : taskText, id: Math.floor(Math.random()* 10000), complete: false }]);
+      setTaskText('')
+    }
+    else{
+      throw new Error('Please add a task')
+    }
+
+  }
+
+  const deleteTask = (id : number) =>{
+    setTasks(tasks.filter(el => el.id !== id))
+  }
+
+  const completeTask = (id: number) =>{
+    setTasks(tasks.map((el) => {
+      if(el.id === id){
+        return {...el, complete: !el.complete};
+      }
+      else{
+        return el;
+      }
+    }))
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <label htmlFor='taskInput'>Task:</label>
+        <input type='text' onChange={(e) => setTaskText(e.target.value)} value={taskText} id='taskInput' placeholder='Task'/>
+        <button onClick={(e) => addTask()}>Add</button>
+      </div>
+
+      {tasks.map((el) =>(
+        <div key={el.id}>
+          <h1 className={el.complete? 'complete' : 'incomplete'}>{el.task}</h1>
+          <button onClick={(e) => deleteTask(el.id)}>Delete</button>
+          <button onClick={(e) => completeTask(el.id)}>{(el.complete ?  'Mark Incomplete' :'Mark Complete')}</button>
+        </div>
+      ))}
+
+    
+    </>
   );
 }
 
